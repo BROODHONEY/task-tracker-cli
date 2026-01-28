2a8cf71fd0b539233ecfda739f5f2655daaf6c86
BROODHONEY <nandeshwar1114@gmail.com>
2026-01-28 10:57:10 +0530

    Task mark-inprogress built

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ if (command === 'add') {
     const newTask = {
         id : tasks.lastId + 1,
         title: title,
+        progress: false,
         done: false
     }
 
@@ -25,13 +26,13 @@ if (command === 'add') {
     console.log(`Added task: "${title}"`);
 }
 else if (command === 'list') {
-    if (tasks.length === 0) {
+    if (tasks.tasks.length === 0) {
         console.log('No tasks found.');
 
     } else {
         console.log('Tasks:');
         tasks.tasks.forEach(task => {
-            console.log(`${task.id}. [${task.done ? 'X' : ' '}] ${task.title}`);
+            console.log(`${task.id}. [${task.done ? 'X' : task.progress ? 'P' : ' '}] ${task.title}`);
         });
     }
 }
@@ -44,11 +45,24 @@ else if (command === 'done') {
         process.exit(1);
     }
     task.done = true;
+    task.progress = false;
     taskHandler.saveTasks(tasks);
     console.log(`Marked task ${id} as done.`);
 
 }
-else if (command === 'del' || command === 'delete') {
+else if (command === 'inp') {
+    const id = Number(args[1]);
+    const task = tasks.tasks.find(t => t.id === id);
+    
+    if (!task) {
+        console.log(`Task with ID ${id} not found.`);
+        process.exit(1);
+    }
+    task.progress = true;
+    taskHandler.saveTasks(tasks);
+    console.log(`Marked task ${id} as in-progress.`);
+}
+else if (command === 'rem' || command === 'remove') {
     const id = Number(args[1]);
     const index = tasks.tasks.findIndex(t => t.id === id);
     
@@ -66,7 +80,9 @@ else if (command === 'help' || !command) {
     console.log('  task add "Task Title"   - Add a new task');
     console.log('  task list                - List all tasks');
     console.log('  task done <id>          - Mark a task as done');
-    console.log('  task del <id>           - Delete a task');
+    console.log('  task inp <id>           - Mark a task as in-progress');
+    console.log('  task rem <id>           - Remove a task');
+    console.log('  task clr-list           - Clear all tasks');
     console.log('  task help               - Show this help message');
 }
 else if (command === 'clr-list' || command === 'clear-list') {
